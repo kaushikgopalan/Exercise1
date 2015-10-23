3cdbb418e378b07727ce660968c89902d2538d50
Kaushik Gopalan <kaushikg14@gmail.com>
2015-10-23 14:35:48 +0200

    Adding to new branch

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,12 @@
+class HelloUser{
+	String userName;
+	public HelloUser(String name){
+		userName=name;
+		
+	}// end constructor HelloUser();
+	
+	public void greetUser(){
+		System.out.println("Hello "+userName+" !");
+		
+	}// method greetUser();
+}// end of class HelloUser;
